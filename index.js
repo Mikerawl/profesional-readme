9d829ca41d37fe0fab6ef8ad23110b9c708389ae
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-16 14:49:40 -0600

    added email function

--- index.js
+++ index.js
@@ -16,32 +16,68 @@ const questions = [ {
     name: "description",
     message: "Please provide a description of your program or project"
     validate: validateInput,
-}
+},
                     {
     type: "input",
     name: "instalation",
     message: "please provide an explanation of how to install or use your program or project.",
     validate: validateInput,
-}
+},
 
                     {
     type: "input",
     name: "usage",
     message: " Please describe how this software is to be used can used.",
     validate: validateInput,
+},
+                    {
+type: "input",
+name: "usage",
+message: "What is your GitHub username?",
+validate: validateInput,
+},
+
+
+                    {
+type: "input",
+name: "usage",
+message: " Please select a license for you project.",
+choic: [],
+validate: validateInput,
+},
+                {
+type: "input",
+name: "userEmail",
+message: "What is your email address for contributor contact?",
+validate: function(value) {
+	var filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if (filter.test(email)) {
+		return true;
+	}
+	return false, "Not a valid email address. Please enter a valid email address";
 }
+},
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if err {
+            return console.log(err);
+        }
+        });
+    }
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt (questions)
-.then(anwers => {
-const readmeContent = generateMarkdown(anwers)
-})
-}
+    inquirer.prompt (questions).then((answers) => {
+        console.log(JSON.stringify(data, null, ""));
+        data.getLicense = getLicense(data.License);
+        writeToFile("./example./README.md",data);
+    }
+});
+
 
 // Function call to initialize app
 init();
