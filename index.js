c0ff8988b7fa4c733441a9214d71cb1e87b12681
Michael Rawlings <rawlingsm99@gmail.com>
2022-01-17 21:23:00 -0600

    "Added open source license badge function"

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown")
 
+// License function
+
+function getLicense(value) {
+    if (value === "GNU GPLv3") {
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+    } else if (value === "GNU AGPLv3") {
+        return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)"
+    } else if (value === "GNU LGPL v3") {
+        return "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)"
+    } else if (value === "Apache 2.0") {
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    } else if (value === "Boost Software1.0") {
+        return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+    } else if (value === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    } else {
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    }
+}
+
 
 // TODO: Create an array of questions for user input
 const questions = [ {
@@ -14,12 +34,12 @@ const questions = [ {
                     {
     type: "input",
     name: "description",
-    message: "Please provide a description of your program or project"
+    message: "Please provide a description of your program or project",
     validate: validateInput,
 },
                     {
     type: "input",
-    name: "instalation",
+    name: "installation",
     message: "please provide an explanation of how to install or use your program or project.",
     validate: validateInput,
 },
@@ -42,7 +62,7 @@ validate: validateInput,
 type: "input",
 name: "usage",
 message: " Please select a license for you project.",
-choic: [],
+choice: [],
 validate: validateInput,
 },
                 {
